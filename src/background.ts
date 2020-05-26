80c2807d35154118a028cd2cc5a19fcae314669f
Prashant Ghimire <prasmire@gmail.com>
2020-05-26 15:56:16 -0500

    changes

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,14 @@
+const version = '1.0';
 
-function polling() {
-    console.log('polling');
-    setTimeout(polling, 1000 * 30);
+chrome.browserAction.onClicked.addListener(function (tab) {
+    console.log('hello click');
+    chrome.debugger.attach({tabId: tab.id}, version, onAttach.bind(null, tab.id));
+});
+
+function onAttach(tabId) {
+    if (chrome.runtime.lastError) {
+        alert(chrome.runtime.lastError.message);
+        return;
+    }
+    chrome.windows.create({url: 'popup.html?' + tabId, type: 'popup', width: 800, height: 600});
 }
-
-polling();
-
--- src/popup.ts
+++ src/popup.ts
@@ -1,32 +1,41 @@
-import * as moment from 'moment';
 import * as $ from 'jquery';
 
-let count = 0;
+let tabId = parseInt(window.location.search.substring(1));
 
-$(function() {
-  const queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    $('#url').text(tabs[0].url);
-    $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
-  });
-
-  chrome.browserAction.setBadgeText({text: count.toString()});
-  $('#countUp').click(()=>{
-    chrome.browserAction.setBadgeText({text: (++count).toString()});
-  });
-
-  $('#changeBackground').click(()=>{
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        color: '#555555'
-      },
-      function(msg) {
-        console.log("result message:", msg);
-      });
-    });
-  });
+window.addEventListener('load', function () {
+    chrome.debugger.sendCommand({tabId: tabId}, 'Network.enable');
+    chrome.debugger.onEvent.addListener(doThing);
 });
+
+window.addEventListener('unload', function () {
+    chrome.debugger.detach({tabId: tabId});
+});
+
+function doThing(debugeeId, message, params) {
+    try {
+        let url = (params.request.url);
+        if (url.indexOf('.capitalone.com') <= -1) {
+            return;
+        }
+        let urlParams = params.request.url.split('&');
+        let items = urlParams
+            .map(item => {
+                let field = '';
+                let string = item.split('=')[1];
+                if (!string || string === 'NaN' || string === 'null') {
+                    field = '<span style="color: red">' + item + '</span>';
+                } else {
+                    field = item;
+                }
+                return decodeURIComponent(field);
+            })
+            .join('<br>');
+        if (message === 'Network.requestWillBeSent') {
+            $('#sent').append(items + '<br>');
+        } else if (message === 'Network.responseReceived') {
+            $('#received').append(items + '<br>');
+        }
+    } catch (e) {
+
+    }
+}
