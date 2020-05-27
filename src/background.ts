62be8c4bb39967be572cafec5ea8920e6f69c5f9
Prashant Ghimire <prashant.ghimire@selu.edu>
2020-05-27 17:05:55 -0500

    central alert

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,17 @@
 const version = '1.0';
-
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.windows.create({url: 'popup.html', type: 'popup', width: 800, height: 600});
+
+  let w = 800;
+  let h = 600;
+  let left = (screen.width / 2) - (w / 2);
+  let top = (screen.height / 2) - (h / 2);
+  chrome.windows.create({
+    url: 'popup.html', type: 'popup',
+    width: w,
+    height: h,
+    left: left,
+    top: top
+  }, function (window) {
+  });
 });
+
