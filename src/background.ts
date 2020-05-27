d6634a0e8d06cb6a81ded15693c82af1aaefb9bc
Prashant Ghimire <prashant.ghimire@selu.edu>
2020-05-27 15:40:58 -0500

    cleanup

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,5 @@
 const version = '1.0';
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log('hello click');
-    chrome.debugger.attach({tabId: tab.id}, version, onAttach.bind(null, tab.id));
+  chrome.windows.create({url: 'popup.html', type: 'popup', width: 800, height: 600});
 });
-
-function onAttach(tabId) {
-    if (chrome.runtime.lastError) {
-        alert(chrome.runtime.lastError.message);
-        return;
-    }
-    chrome.windows.create({url: 'popup.html?' + tabId, type: 'popup', width: 800, height: 600});
-}
--- src/popup.ts
+++ src/popup.ts
@@ -1,41 +1,12 @@
-import * as $ from 'jquery';
+let callback = function (details) {
+  $('#dealerInfo').append(details.method + ' -- ' +details.url + '<br>');
+};
 
-let tabId = parseInt(window.location.search.substring(1));
+let filter = {urls: ['https://*.capitalone.com/*'],};
+let extraInfoSpec = ['requestBody'];
 
-window.addEventListener('load', function () {
-    chrome.debugger.sendCommand({tabId: tabId}, 'Network.enable');
-    chrome.debugger.onEvent.addListener(doThing);
+chrome.webRequest.onBeforeRequest.addListener(callback, filter, extraInfoSpec);
+
+$(document).on('click', '#button1', function () {
+  console.log('loaded click');
 });
-
-window.addEventListener('unload', function () {
-    chrome.debugger.detach({tabId: tabId});
-});
-
-function doThing(debugeeId, message, params) {
-    try {
-        let url = (params.request.url);
-        if (url.indexOf('.capitalone.com') <= -1) {
-            return;
-        }
-        let urlParams = params.request.url.split('&');
-        let items = urlParams
-            .map(item => {
-                let field = '';
-                let string = item.split('=')[1];
-                if (!string || string === 'NaN' || string === 'null') {
-                    field = '<span style="color: red">' + item + '</span>';
-                } else {
-                    field = item;
-                }
-                return decodeURIComponent(field);
-            })
-            .join('<br>');
-        if (message === 'Network.requestWillBeSent') {
-            $('#sent').append(items + '<br>');
-        } else if (message === 'Network.responseReceived') {
-            $('#received').append(items + '<br>');
-        }
-    } catch (e) {
-
-    }
-}
